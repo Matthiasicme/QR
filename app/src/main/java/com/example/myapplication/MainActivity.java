893ab95466774159c3d03dddaa25892b230de602
Matthiasicme <matthias602.n@gmail.com>
2023-11-17 15:58:29 +0100

    first idea

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 101;
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startScanner();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void startScanner() {
+        scannerView = findViewById(R.id.scannerView);
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(com.google.zxing.Result result) {
+        // tu sie doda wynik skanowania, zeby sie wyswietlil na ekranie ect
+        Toast.makeText(this, "Zeskanowano: " + result.getText(), Toast.LENGTH_LONG).show();
+        scannerView.resumeCameraPreview(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (scannerView != null) {
+            scannerView.stopCamera();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (scannerView != null) {
+            scannerView.setResultHandler(this);
+            scannerView.startCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScanner();
+            } else {
+                Toast.makeText(this, "Brak uprawnień do kamery", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
